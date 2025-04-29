0226205871f71101c7311bd1abc4886a08a1a056
Tanusree1308 <24pa1a0565@vishnu.edu.in>
2025-04-29 12:05:35 +0530

    Update server.js

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const bodyParser = require('body-parser');
 const { MongoClient } = require('mongodb');
 
 const app = express();
-const port = 3000;
+const port = 3001;
 
 // Middlewares
 app.use(cors());
